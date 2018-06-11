e771ef2038a57bcb8864ba1d2592a04d4a9e8059
downjoy <downjoy@mac-mini.local>
2018-06-11 19:55:14 +0800

    优化

--- AppStorePayDemo/AppDelegate.m
+++ AppStorePayDemo/AppDelegate.m
@@ -18,7 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    [[AppStoreKit getInstance] initAppStoreWithSuiteName:@"group.westbundgame" clear:false];
+    [[AppStoreKit getInstance] initAppStoreWithSuiteName:@"group.yoursuitename" clear:false];
     return YES;
 }
 
